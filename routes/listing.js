8d12db63dc55d1c8dcbee93413f85cfc2afbf625
Abhiraj kumar <nishukumarabhiraj@gmail.com>
2026-07-09 01:32:50 +0530

    final fix for cloudConfig path

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,7 @@ const{isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listing.js");
 const multer  = require('multer');
-const{storage} = require("../cloudconfig.js");
+const{storage} = require("../cloudConfig.js");
 const upload = multer({ storage });
 
 router.
